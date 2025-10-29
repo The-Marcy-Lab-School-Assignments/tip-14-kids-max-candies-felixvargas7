b56bce52ea97dd299cbb350f7ea7475b545d3052
Felix Vargas <felix70529@gmail.com>
2025-10-29 14:18:54 -0400

    Worked on tip-14

--- index.js
+++ index.js
@@ -10,7 +10,23 @@
  * @returns {boolean[]}
  */
 function kidsWithCandies(candies, extraCandies) {
+    // Whats going on here? why are we doing this?
+    // In this function, we are creating a new variable "greatest" 
+    // that essentially finds the max in the "candies" parameter
+    // array, using the Math.max method. We also use the spread operator
+    // on the parameter "candies" to iterate through all the elements 
+    // in the array. 
 
+    let greatest = Math.max(...candies);
+
+
+    // Whats going on here? why are we doing this?
+    // We now return the "candies" array using the map method, which 
+    // allows us to make a new array with the same amount of elements. We
+    // then check inside map if "el" parameter for candies as the callback 
+    // plus extraCandies is greater than or equal to the greatest
+    // 
+    return candies.map((el) => el + extraCandies >= greatest);
 }
 
 module.exports = kidsWithCandies;
